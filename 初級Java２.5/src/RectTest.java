8f64a6e72b9f581702f67f619a0d07b4ee7bfcc7
kuroro <gm27360123@gmail.com>
2024-05-14 12:12:05 +0900

    3.3解きました

--- 初級Java２.5/src/RectTest.java
+++ 初級Java２.5/src/RectTest.java
@@ -2,11 +2,18 @@
 public class RectTest extends MyFrame {
 	public void run() {
 		
+		
+		
+		setColor(255,255,0);
 		fillRect(30,40,50,100);
+		setColor(200,0,255);
 		fillRect(130,40,50,100);
+		setColor(128,128,128);
 		fillRect(30,200,150,10);
-		fillRect(90,160,30,30);
-	
+		setColor(126,211,255);
+		fillRect(60,40,50,100);
+		setColor(0,0,0);
+		fillRect(130,60,50,100);
 	}
 	
 	}
