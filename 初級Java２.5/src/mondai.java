8054ce394d9b52bbc6cf0e2ce53e7abb7db8a2bc
kuroro <gm27360123@gmail.com>
2024-05-20 11:16:30 +0900

    練習問題４－２　解決

---
+++ 初級Java２.5/src/mondai.java
@@ -0,0 +1,19 @@
+
+public class mondai extends MyFrame{
+	public void run() {
+		setColor(0,128,0);
+		int x = 30;
+		while(x <=210) {
+			
+			fillRect(x,80,10,100);
+			
+			x+=20;
+		}
+		
+		
+		
+		
+		
+		
+	}
+}
