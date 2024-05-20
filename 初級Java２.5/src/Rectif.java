2e3fd37c481081c360af467cb8cb1cd0661314db
kuroro <gm27360123@gmail.com>
2024-05-20 12:08:59 +0900

    4.4zikkou

---
+++ 初級Java２.5/src/Rectif.java
@@ -0,0 +1,35 @@
+
+public class Rectif extends MyFrame {
+	public void run() {
+		setColor(0,128,0);
+		int x = 30;
+		int i;
+		for(i = 0; i<9; i++) {
+			if(i >= 4 && i <=7) {
+				
+				fillRect(x,100,10,100);
+				
+			}
+			else {
+				fillRect(x,80,10,100);
+			}
+			x+=20;
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+			
+
+}
