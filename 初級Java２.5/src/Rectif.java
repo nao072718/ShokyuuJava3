6d1a2ef00ee4d6ab4eea44996f572c578fe21e22
kuroro <gm27360123@gmail.com>
2024-05-21 10:20:35 +0900

    練習問題４－２解きました

--- 初級Java２.5/src/Rectif.java
+++ 初級Java２.5/src/Rectif.java
@@ -1,16 +1,15 @@
 
+
 public class Rectif extends MyFrame {
 	public void run() {
 		setColor(0,128,0);
 		int x = 30;
-		int y = 30;
+		int y = 10;
 		int i;
 		for(i = 0; i<9; i++) {
-			fillRect(x,y,10,100);
-			
+			fillRect(x,80,10,y);
 			x+=20;
 			y+=20;
-			
 			
 			
 			
@@ -28,3 +27,4 @@ public class Rectif extends MyFrame {
 			
 
 }
+
