fb5be230fe7f21fa5a6227b4f3a246b509ce3ccb
kuroro <gm27360123@gmail.com>
2024-05-28 10:38:45 +0900

    6-3完了

--- 初級Java２.5/src/DiaplayFaceObject.java
+++ 初級Java２.5/src/DiaplayFaceObject.java
@@ -1,11 +1,22 @@
 
 public class DiaplayFaceObject extends MyFrame{
 	public void run() {
-		Face face1 = new Face();
-		face1.x=100;
-		face1.y=50;
-		face1.draw(this);
-		
+		Face face1 = new Face(50,50);
+		Face face2 = new Face(200,100);
+		for(int i=0;i<30;i++) {
+			clear();
+			face1.draw(this);
+			face2.draw(this);
+			face1.x+=10;
+			face1.y+=5;
+			face2.x-=10;
+			face2.y-=5;
+			sleep(0.1);
+			
+			
+			
+			
+		}
 		
 		
 		
