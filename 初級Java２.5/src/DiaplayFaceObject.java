dc8d7d32963f34db6de2fff6c7cbca935616a961
kuroro <gm27360123@gmail.com>
2024-05-28 10:13:09 +0900

    確認問題6－2解きました

--- 初級Java２.5/src/DiaplayFaceObject.java
+++ 初級Java２.5/src/DiaplayFaceObject.java
@@ -1,12 +1,12 @@
 
 public class DiaplayFaceObject extends MyFrame{
 	public void run() {
-		Face face1 = new Face();
-		face1.x=100;
-		face1.y=50;
+		Face face1 = new Face(25,50);
 		face1.draw(this);
-		
-		
+		Face face2 = new Face(125,150);
+		face2.draw(this);
+		Face face3 = new Face(225,50);
+		face3.draw(this);
 		
 		
 		
