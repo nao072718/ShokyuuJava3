1c0505f9eac484c862a9b0000508e40f65de6d3a
kuroro <gm27360123@gmail.com>
2024-06-03 10:07:13 +0900

    練習問題6－2解きました

---
+++ 初級Java２.5/src/Displaycar.java
@@ -0,0 +1,7 @@
+
+public class Displaycar extends MyFrame{
+	public void run() {
+		car car1= new car();
+		car1.draw(this);
+	}
+}
---
+++ 初級Java２.5/src/car.java
@@ -0,0 +1,15 @@
+
+public class car {
+	public void draw(MyFrame frame) {
+		frame.fillOval(60, 210, 35, 35);
+		frame.fillOval(120, 210, 35, 35);
+		frame.fillRect(70, 150, 90, 50);
+		frame.fillRect(50,180, 130, 40);
+		
+	}
+	
+	
+	
+	
+	
+}
