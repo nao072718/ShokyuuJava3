b9e6335c089f688824caaf9b75cf2dbeaaa15baa
kuroro <gm27360123@gmail.com>
2024-06-10 10:04:27 +0900

    8－2解きました

---
+++ 初級Java２.5/src/DisplayCarTrain.java
@@ -0,0 +1,12 @@
+
+public class DisplayCarTrain extends MyFrame{
+	public void run() {
+		Nextcar car1=new Nextcar(10,50,3,0);
+		car1.draw(this);
+		Train train1=new Train(30,150,3,0);
+		train1.draw(this);
+		
+		
+		
+	}
+}
